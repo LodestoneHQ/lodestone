3ba50869d5dce258e720095300edea382a2e249d
Jason Kulatunga <jason@thesparktree.com>
2019-08-25 15:34:29 -0700

    working docx viewer.

--- web/src/app/details/details.component.ts
+++ web/src/app/details/details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ElasticsearchService} from "../services/elasticsearch.service";
 import {ActivatedRoute} from "@angular/router";
 import {SearchResult} from "../models/search-result";
+import * as mammoth from '../../../node_modules/mammoth/mammoth.browser.js';
+import {Observable} from "rxjs";
+import {HttpClient, HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-details',
@@ -12,7 +15,7 @@ export class DetailsComponent implements OnInit {
 
   documentId: string;
   documentData: SearchResult = new SearchResult();
-  constructor(private es: ElasticsearchService, private activatedRoute: ActivatedRoute) { }
+  constructor(private es: ElasticsearchService, private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
     this.documentId = this.activatedRoute.snapshot.params.id;
@@ -21,6 +24,10 @@ export class DetailsComponent implements OnInit {
       .subscribe(
         data => {
           this.documentData = data;
+
+          if(this.documentData._source.file.extension == 'docx' || this.documentData._source.file.extension == 'doc'){
+            this.displayDocxFile()
+          }
         },
         error => console.error(error),
         () => console.log("FINISHED")
@@ -50,6 +57,45 @@ export class DetailsComponent implements OnInit {
           console.log("update finished")
         }
       );
+  }
+
+  displayDocxFile(){
+    this.getStaticFile('/static' + this.documentData._source.path.virtual)
+
+  }
+
+
+  getStaticFile (filePath) {
+    return this.http.get(filePath, {
+      responseType: 'arraybuffer',
+      observe: 'response'
+    })
+
+      .subscribe(
+        arrBuffResp => {
+          console.log("SUCCESSFUL ARRYBUFFER")
+          this.renderDocx(arrBuffResp.body)
+        },
+        err => console.error(err),
+        () => {
+          console.log("COMPLETE")
+        }
+      )
+  }
+
+  renderDocx(data:ArrayBuffer) {
+    console.log("REQUESTING MAMMOTH DOCUMENT.");
+    mammoth.convertToHtml({arrayBuffer: data})
+      .then((result) => {
+        document.getElementById("output").innerHTML = result.value;
+        console.log(result.messages)
+        // var messageHtml = result.messages.map(function(message) {
+        //   return '<li class="' + message.type + '">' + message.message + "</li>";
+        // }).join("");
+        //
+        // document.getElementById("messages").innerHTML = "<ul>" + messageHtml + "</ul>";
+      })
+      .done();
   }
 
 }
