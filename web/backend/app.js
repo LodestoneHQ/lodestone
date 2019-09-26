1e8004442ab14b9faa5613f4125f4b845f4e281e
Jason Kulatunga <jason@thesparktree.com>
2019-09-25 18:33:44 -0700

    web frontend now accessible via /web url. api at /api/v1/. fixing asset urls to be relative so they load correctly via /web deploy url. Fixed download and preview urls to use /storage/documents . Added build and watch command for frontend to the npm file for easy updating. Renamed network and volume names in docker-compose file. added traefik routing via labels to web frontend and api.

--- web/backend/app.js
+++ web/backend/app.js
@@ -12,9 +12,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/web',express.static(path.join(__dirname, 'public')));
+app.use('/web', indexRouter);
 
 app.use('/api/v1/users', usersRouter);
-app.use('/', indexRouter);
 
 module.exports = app;
