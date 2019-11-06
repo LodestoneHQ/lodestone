6d1793dadd426dfefedda629f55fada51ec850b8
Jason Kulatunga <jason@thesparktree.com>
2019-11-05 21:04:30 -0800

    WIP: replace fscrawler with lodestone custom processor engine.

---
+++ processor/rootfs/lodestone-processor-dl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# retrieve the latest lodestone-fs-publisher release info
+asset_url=$(curl -L -s https://api.github.com/repos/AnalogJ/lodestone-processor/releases/latest \
+	| grep browser_download_url | grep 'lodestone-processor-linux-amd64' | cut -d '"' -f 4)
+
+# download the lodestone-processor asset here.
+curl -L -o /usr/bin/lodestone-processor $asset_url
+
+# make lodestone-processor executable
+chmod +x /usr/bin/lodestone-processor
