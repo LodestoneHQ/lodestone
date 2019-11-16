22d0645e3448d0d0f9f249f9c0829caecf933a30
Jason Kulatunga <jason@thesparktree.com>
2019-11-16 15:18:57 -0800

    fixing document processor.

--- processor/rootfs/lodestone-processor-dl.sh
+++ processor/rootfs/lodestone-processor-dl.sh
@@ -5,7 +5,7 @@ thumb_url=$(curl -L -s https://api.github.com/repos/AnalogJ/lodestone-processor/
 	| grep browser_download_url | grep 'lodestone-thumbnail-processor-linux-amd64' | cut -d '"' -f 4)
 
 doc_url=$(curl -L -s https://api.github.com/repos/AnalogJ/lodestone-processor/releases/latest \
-	| grep browser_download_url | grep 'lodestone-thumbnail-processor-linux-amd64' | cut -d '"' -f 4)
+	| grep browser_download_url | grep 'lodestone-document-processor-linux-amd64' | cut -d '"' -f 4)
 
 # download the lodestone-processor asset here.
 curl -L -o /usr/bin/lodestone-thumbnail-processor $thumb_url
@@ -14,5 +14,9 @@ curl -L -o /usr/bin/lodestone-document-processor $doc_url
 # make lodestone-processor executable
 chmod +x /usr/bin/lodestone-thumbnail-processor
 chmod +x /usr/bin/lodestone-document-processor
+
+# ensure that the required dependencies are available (imagemagick for thumbnail-processor and tika for document-processor)
+pkg-config --cflags --libs MagickWand
+
 /usr/bin/lodestone-thumbnail-processor --version
 /usr/bin/lodestone-document-processor --version
