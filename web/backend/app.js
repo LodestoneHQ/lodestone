7512138dbb15583f8c60b4400e4620838cac1bbc
Jason Kulatunga <jason@thesparktree.com>
2020-02-12 21:46:22 -0800

    increasing the max "upload" size for elasticsearch. Adding a filetypes.json file specifying extensions to ignore. Adding a API endpoint for uploading files to storage (streaming) Reorganized the App.js file for express. Adding .dockerignore to ensure that node_modules are correctly ignored when building locally.

--- web/backend/app.js
+++ web/backend/app.js
@@ -3,6 +3,7 @@ var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,19 +14,19 @@ var storageRouter = require('./routes/storage');
 
 var app = express();
 
-app.set('json spaces', 2);
-
+app.use(cors())
 app.use(logger('dev'));
+app.use('/api/v1/storage', storageRouter);
+
+
+app.set('json spaces', 2);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors())
+
 app.use('/web',express.static(path.join(__dirname, 'public')));
 app.use('/web', indexRouter);
-
-
 app.use('/api/v1/data', express.static(path.join(__dirname, 'data')));
-app.use('/api/v1/storage', storageRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/status', statusRouter);
 app.use('/api/v1/elastic', elasticRouter);
--- web/backend/routes/storage.js
+++ web/backend/routes/storage.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser')
 var AWS = require('aws-sdk');
 var _ = require('lodash');
 
@@ -26,9 +27,32 @@ AWS.Request.prototype.forwardToExpress = function forwardToExpress(res, next) {
 /* GET file from minio, authentication handled server-side. Since minio is not exposed, and buckets
  are still protected via access/secret key its better protected.  */
 //https://stackoverflow.com/questions/35782434/streaming-file-from-s3-with-express-including-information-on-length-and-filetype
-router.get('/:bucket*', function(req, res, next) {
+router.get('/:bucket/*', function(req, res, next) {
     s3.getObject({Bucket: req.params.bucket, Key: req.params['0']})
         .forwardToExpress(res, next);
 });
+
+/* POST file to minio, authentication handled server-side. Since minio is not exposed, and buckets
+ are still protected via access/secret key its better protected.  */
+router.post('/:bucket/*', function(req, res, next) {
+
+    const params = {
+        Bucket: req.params.bucket,
+        Key: req.params['0'],
+        Body: req, //req is a stream
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if(err){
+            res.send('Error Uploading Data: ' + JSON.stringify(err) + '\n' + JSON.stringify(err.stack));
+
+        } else {
+            console.log(`File uploaded successfully. ${data.Location}`);
+            res.json({"status":"success"})
+        }
+    });
+});
+
 
 module.exports = router;
